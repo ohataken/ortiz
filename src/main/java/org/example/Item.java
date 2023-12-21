e7081281a1183150b453180170bc3784eb1626ad
ohataken <11092425+ohataken@users.noreply.github.com>
2023-12-22 01:23:48 +0900

    add Item, POJO

---
+++ src/main/java/org/example/Item.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class Item {
+
+    private Integer id;
+    private Integer value;
+    private Integer weight;
+    private Knapsack knapsack;
+
+    Item(Integer id, Integer value, Integer weight) {
+        this.id = id;
+        this.value = value;
+        this.weight = weight;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public Integer getValue() {
+        return this.value;
+    }
+
+    public Integer getWeight() {
+        return this.weight;
+    }
+
+    public Knapsack getKnapsack() {
+        return this.knapsack;
+    }
+
+    public void setKnapsack(Knapsack knapsack) {
+        this.knapsack = knapsack;
+    }
+
+}
