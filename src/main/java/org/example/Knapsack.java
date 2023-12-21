50da558a022ac59f23f75eafc800ea3da27a9cc6
ohataken <11092425+ohataken@users.noreply.github.com>
2023-12-22 01:23:56 +0900

    add Knapsack, POJO

---
+++ src/main/java/org/example/Knapsack.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Knapsack {
+
+    private Integer id;
+    private Integer capacity;
+
+    Knapsack(Integer id, Integer capacity) {
+        this.id = id;
+        this.capacity = capacity;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public Integer getCapacity() {
+        return this.capacity;
+    }
+
+}
