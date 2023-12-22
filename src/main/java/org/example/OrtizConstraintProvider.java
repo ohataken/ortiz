019b4055e910fdef97596f4c34b14c4e850f3474
ohataken <11092425+ohataken@users.noreply.github.com>
2023-12-22 14:48:50 +0900

    penalizeKnapsackCapacityOver

--- src/main/java/org/example/OrtizConstraintProvider.java
+++ src/main/java/org/example/OrtizConstraintProvider.java
@@ -3,13 +3,26 @@ package org.example;
 import org.optaplanner.core.api.score.stream.ConstraintProvider;
 import org.optaplanner.core.api.score.stream.ConstraintFactory;
 import org.optaplanner.core.api.score.stream.Constraint;
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+
+import static org.optaplanner.core.api.score.stream.ConstraintCollectors.sum;
 
 public class OrtizConstraintProvider implements ConstraintProvider {
 
     @Override
     public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
         return new Constraint[] {
+                penalizeKnapsackCapacityOver(constraintFactory),
         };
+    }
+
+    Constraint penalizeKnapsackCapacityOver(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .forEach(Item.class)
+                .filter((item) -> item.belongsToKnapsack())
+                .groupBy(Item::getKnapsack, sum(Item::getWeight))
+                .penalize(HardSoftScore.ONE_HARD, (knapsack, weight) -> knapsack.getCapacity() < weight ? weight - knapsack.getCapacity() : 0)
+                .asConstraint("penalize knapsack capacity over");
     }
 
 }
